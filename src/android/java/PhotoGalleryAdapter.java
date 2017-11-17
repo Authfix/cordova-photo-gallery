dad44dcdc0a2068a161d4c3d969bdbc899d53c47
Authfix <3940727+Authfix@users.noreply.github.com>
2017-11-17 13:47:47 +0100

    fix(android): add missing log import
    Add missing log import

--- src/android/java/PhotoGalleryAdapter.java
+++ src/android/java/PhotoGalleryAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.util.Log;
 
 import com.github.chrisbanes.photoview.PhotoView;
 import com.squareup.picasso.Picasso;
